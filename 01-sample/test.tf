56058ae93798a7a97117dad264e06e6929b0537c
hyder18 <Shammijb123@gmail.com>
2023-06-26 14:37:50 +0530

    roboshop-component

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2" {
-  ami = "Centos-8-DevOps-Practice"
+  ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ "sg-0587bddaf9ce3d6fb"]
   tags = {
-    name = test
+    name = "test"
   }
 }
