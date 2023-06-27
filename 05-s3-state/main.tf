796ec0803d3844fe6b1bd6f3b996baf188ae7597
hyder18 <Shammijb123@gmail.com>
2023-06-27 16:51:22 +0530

    state-file

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -7,10 +7,11 @@ terraform {
 }
 
 resource "aws_instance" "ec2" {
-  ami = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [ "sg-0587bddaf9ce3d6fb"]
-  tags = {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0587bddaf9ce3d6fb"]
+  tags                   = {
     name = "test"
   }
+
 }
