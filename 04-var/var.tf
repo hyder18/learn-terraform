32f290fd6ed0209d6a65529f99fc089ec2e15e6e
hyder18 <Shammijb123@gmail.com>
2023-06-27 14:12:20 +0530

    learn-terraform

--- 04-var/var.tf
+++ 04-var/var.tf
@@ -11,3 +11,29 @@ variable "hyder" {}
 output "hyder" {
   value = var.hyder
 }
+
+variable "sample5" {
+  default = "hyder"
+}
+
+variable "sample6" {
+  default = [
+  "hello",
+    "world",
+    1000,
+    true
+  ]
+}
+
+variable "sample7" {
+  default = {
+    name = "shami"
+    job = "working"
+    salary = 25000
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "variable sample5 - ${var.sample5},first value of the list ${var.sample6[0]},boolean value of map - ${var.sample7["boolean"]}"
+}
