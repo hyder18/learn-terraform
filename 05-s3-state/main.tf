8cf35508198e49cd85b7d09cbfab5d70ee0c8ec9
hyder18 <Shammijb123@gmail.com>
2023-06-27 16:49:57 +0530

    roboshop-setup

---
+++ 05-s3-state/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-hyder71"
+    key    = "05-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-0587bddaf9ce3d6fb"]
+  tags = {
+    name = "test"
+  }
+}
