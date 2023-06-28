df6d1603b81e427a5476592268fe91ce794dccfa
hyder18 <Shammijb123@gmail.com>
2023-06-28 13:47:47 +0530

    roboshop-setup

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  for_each = var.instances
+  ami                    = "ami-03265a0778a880afb"
+  instance_type           = each.value["type"]
+  vpc_security_group_ids = [ "sg-0587bddaf9ce3d6fb"]
+  tags = {
+    name = each.value["name"]
+  }
+
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      instance_type = "t3.small"
+    }
+  }
+}
