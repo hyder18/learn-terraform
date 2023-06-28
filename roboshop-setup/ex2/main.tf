0eff33f385fcc754d2672b78d9d9146e00c4bff7
hyder18 <Shammijb123@gmail.com>
2023-06-28 13:17:27 +0530

    roboshop-setup

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -9,6 +9,9 @@ resource "aws_instance" "frontend" {
   ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ "sg-0587bddaf9ce3d6fb"]
+  tags = {
+    name = var.instances[count.index]
+  }
 
 }
 
