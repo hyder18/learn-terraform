c5620250873df153a2fc9b68dcdc043317c8dd7a
hyder18 <Shammijb123@gmail.com>
2023-06-30 15:43:54 +0530

    roboshop-setup

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "record" {
   name    = "${var.component}-dev.hyder71.online"
   type    = "A"
   ttl     = 30
-  records = var.private_ip
+  records = [var.private_ip]
 }
 
 variable "private_ip" {}
